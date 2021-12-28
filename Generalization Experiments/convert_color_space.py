9eb2619b978214efd58dec961b99239f78b8ef83
Jeonghan Lee (Francis) <77098071+Jeonghan57@users.noreply.github.com>
2021-12-28 16:54:34 +0900

    Add files via upload

---
+++ Generalization Experiments/convert_color_space.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 29 17:59:47 2021
+
+@author: IVCML
+"""
+
+import cv2
+from tqdm import tqdm
+import os
+"""
+# Adding Set
+for i in range (2, 11):
+    in_path = f"./Datafolder/Add/train(Add_" + str(i) +")/fake/"
+    out_path = f"./Datafolder/Add/train(Add_" + str(i) + ")(HSV)/fake/"
+    
+    
+    for img_name in tqdm(os.listdir(in_path)):
+        img = cv2.imread(os.path.join(in_path, img_name))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        cv2.imwrite(os.path.join(out_path, img_name), img)
+"""
+
+# Test Set
+in_path = f"./Datafolder/FFHQ/test(StyleGAN2)/fake/"
+out_path = f"./Datafolder/FFHQ/test(StyleGAN2)(HSV)/fake/"
+
+for img_name in tqdm(os.listdir(in_path)):
+    img = cv2.imread(os.path.join(in_path, img_name))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    cv2.imwrite(os.path.join(out_path, img_name), img)
+
